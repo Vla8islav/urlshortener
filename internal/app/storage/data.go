93b60b8bdb7f25d5eec60eea6c7d19088ce7bca8
Vladislav Pilipenko <vladislav.pilipenko@sravni.ru>
2024-01-14 18:50:06 +0300

    Fixed race conditions

--- internal/app/storage/data.go
+++ internal/app/storage/data.go
@@ -3,15 +3,16 @@ package storage
 import "sync"
 
 var instance *makeshiftStorage = nil
+var mu sync.Mutex
 
 func GetInstance() MakeshiftStorage {
-	sync.OnceFunc(func() {
-		if instance == nil {
-			instance = new(makeshiftStorage)
-			instance.urlToShort = make(map[string]string)
-			instance.shortToURL = make(map[string]string)
-		}
-	})()
+	mu.Lock()
+	if instance == nil {
+		instance = new(makeshiftStorage)
+		instance.urlToShort = make(map[string]string)
+		instance.shortToURL = make(map[string]string)
+	}
+	mu.Unlock()
 
 	return instance
 }
@@ -28,16 +29,22 @@ type makeshiftStorage struct {
 }
 
 func (s makeshiftStorage) AddURLPair(shortenedURL string, fullURL string) {
+	mu.Lock()
 	s.urlToShort[fullURL] = shortenedURL
 	s.shortToURL[shortenedURL] = fullURL
+	mu.Unlock()
 }
 
 func (s makeshiftStorage) GetFullURL(shortenedURL string) (string, bool) {
+	mu.Lock()
 	value, exists := s.shortToURL[shortenedURL]
+	mu.Unlock()
 	return value, exists
 }
 
 func (s makeshiftStorage) GetShortenedURL(fullURL string) (string, bool) {
+	mu.Lock()
 	value, exists := s.urlToShort[fullURL]
+	mu.Unlock()
 	return value, exists
 }
