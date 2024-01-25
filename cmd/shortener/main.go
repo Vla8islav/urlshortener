36acbbf04a954eeb0bfcc5e21a361277f6a28002
Vladislav Pilipenko <vladislav.pilipenko@sravni.ru>
2024-01-26 02:17:03 +0300

    Added some logging into the handlers

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -12,6 +12,35 @@ import (
 
 var sugar zap.SugaredLogger
 
+type (
+	// берём структуру для хранения сведений об ответе
+	responseData struct {
+		status int
+		size   int
+		body   []byte
+	}
+
+	// добавляем реализацию http.ResponseWriter
+	loggingResponseWriter struct {
+		http.ResponseWriter // встраиваем оригинальный http.ResponseWriter
+		responseData        *responseData
+	}
+)
+
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+	// записываем ответ, используя оригинальный http.ResponseWriter
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size // захватываем размер
+	r.responseData.body = b
+	return size, err
+}
+
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+	// записываем код статуса, используя оригинальный http.ResponseWriter
+	r.ResponseWriter.WriteHeader(statusCode)
+	r.responseData.status = statusCode // захватываем код статуса
+}
+
 func WithLogging(h http.HandlerFunc) http.HandlerFunc {
 	logFn := func(w http.ResponseWriter, r *http.Request) {
 		// функция Now() возвращает текущее время
@@ -22,8 +51,17 @@ func WithLogging(h http.HandlerFunc) http.HandlerFunc {
 		// метод запроса
 		method := r.Method
 
+		responseData := &responseData{
+			status: 0,
+			size:   0,
+		}
+		lw := loggingResponseWriter{
+			ResponseWriter: w,
+			responseData:   responseData,
+		}
+
 		// точка, где выполняется хендлер pingHandler
-		h.ServeHTTP(w, r) // обслуживание оригинального запроса
+		h.ServeHTTP(&lw, r) // обслуживание оригинального запроса
 
 		// Since возвращает разницу во времени между start
 		// и моментом вызова Since. Таким образом можно посчитать
@@ -35,6 +73,9 @@ func WithLogging(h http.HandlerFunc) http.HandlerFunc {
 			"uri", uri,
 			"method", method,
 			"duration", duration,
+			"status", responseData.status, // получаем перехваченный код статуса ответа
+			"size", responseData.size, // получаем перехваченный размер ответа
+			"body", string(responseData.body),
 		)
 
 	}
