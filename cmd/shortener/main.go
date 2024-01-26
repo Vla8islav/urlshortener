1872ebec48993d03809e674777f98955a072b3aa
Vladislav Pilipenko <vladislav.pilipenko@sravni.ru>
2024-01-26 21:51:24 +0300

    iter6 Moved logging into the internal

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -4,83 +4,11 @@ import (
 	"github.com/Vla8islav/urlshortener/internal/app"
 	"github.com/Vla8islav/urlshortener/internal/app/configuration"
 	"github.com/Vla8islav/urlshortener/internal/app/handlers"
+	"github.com/Vla8islav/urlshortener/internal/app/logging"
 	"github.com/gorilla/mux"
 	"go.uber.org/zap"
 	"net/http"
-	"time"
 )
-
-var sugar zap.SugaredLogger
-
-type (
-	// берём структуру для хранения сведений об ответе
-	responseData struct {
-		status int
-		size   int
-		body   []byte
-	}
-
-	// добавляем реализацию http.ResponseWriter
-	loggingResponseWriter struct {
-		http.ResponseWriter // встраиваем оригинальный http.ResponseWriter
-		responseData        *responseData
-	}
-)
-
-func (r *loggingResponseWriter) Write(b []byte) (int, error) {
-	// записываем ответ, используя оригинальный http.ResponseWriter
-	size, err := r.ResponseWriter.Write(b)
-	r.responseData.size += size // захватываем размер
-	r.responseData.body = b
-	return size, err
-}
-
-func (r *loggingResponseWriter) WriteHeader(statusCode int) {
-	// записываем код статуса, используя оригинальный http.ResponseWriter
-	r.ResponseWriter.WriteHeader(statusCode)
-	r.responseData.status = statusCode // захватываем код статуса
-}
-
-func WithLogging(h http.HandlerFunc) http.HandlerFunc {
-	logFn := func(w http.ResponseWriter, r *http.Request) {
-		// функция Now() возвращает текущее время
-		start := time.Now()
-
-		// эндпоинт /ping
-		uri := r.RequestURI
-		// метод запроса
-		method := r.Method
-
-		responseData := &responseData{
-			status: 0,
-			size:   0,
-		}
-		lw := loggingResponseWriter{
-			ResponseWriter: w,
-			responseData:   responseData,
-		}
-
-		// точка, где выполняется хендлер pingHandler
-		h.ServeHTTP(&lw, r) // обслуживание оригинального запроса
-
-		// Since возвращает разницу во времени между start
-		// и моментом вызова Since. Таким образом можно посчитать
-		// время выполнения запроса.
-		duration := time.Since(start)
-
-		// отправляем сведения о запросе в zap
-		sugar.Infoln(
-			"uri", uri,
-			"method", method,
-			"duration", duration,
-			"status", responseData.status, // получаем перехваченный код статуса ответа
-			"size", responseData.size, // получаем перехваченный размер ответа
-			"body", string(responseData.body),
-		)
-
-	}
-	return logFn
-}
 
 func main() {
 
@@ -93,13 +21,13 @@ func main() {
 	defer logger.Sync()
 
 	// делаем регистратор SugaredLogger
-	sugar = *logger.Sugar()
+	logging.Sugar = *logger.Sugar()
 
 	short := app.NewURLShortenService()
 
 	r := mux.NewRouter()
-	r.HandleFunc("/", WithLogging(handlers.RootPageHandler(short)))
-	r.HandleFunc("/{slug:[A-Za-z]+}", WithLogging(handlers.ExpandHandler(short)))
+	r.HandleFunc("/", logging.WithLogging(handlers.RootPageHandler(short)))
+	r.HandleFunc("/{slug:[A-Za-z]+}", logging.WithLogging(handlers.ExpandHandler(short)))
 
 	err := http.ListenAndServe(configuration.ReadFlags().ServerAddress, r)
 	if err != nil {
---
+++ internal/app/logging/logging.go
@@ -0,0 +1,79 @@
+package logging
+
+import (
+	"go.uber.org/zap"
+	"net/http"
+	"time"
+)
+
+var Sugar zap.SugaredLogger
+
+type (
+	// берём структуру для хранения сведений об ответе
+	responseData struct {
+		status int
+		size   int
+		body   []byte
+	}
+
+	// добавляем реализацию http.ResponseWriter
+	loggingResponseWriter struct {
+		http.ResponseWriter // встраиваем оригинальный http.ResponseWriter
+		responseData        *responseData
+	}
+)
+
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+	// записываем ответ, используя оригинальный http.ResponseWriter
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size // захватываем размер
+	r.responseData.body = b
+	return size, err
+}
+
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+	// записываем код статуса, используя оригинальный http.ResponseWriter
+	r.ResponseWriter.WriteHeader(statusCode)
+	r.responseData.status = statusCode // захватываем код статуса
+}
+
+func WithLogging(h http.HandlerFunc) http.HandlerFunc {
+	logFn := func(w http.ResponseWriter, r *http.Request) {
+		// функция Now() возвращает текущее время
+		start := time.Now()
+
+		// эндпоинт /ping
+		uri := r.RequestURI
+		// метод запроса
+		method := r.Method
+
+		responseData := &responseData{
+			status: 0,
+			size:   0,
+		}
+		lw := loggingResponseWriter{
+			ResponseWriter: w,
+			responseData:   responseData,
+		}
+
+		// точка, где выполняется хендлер pingHandler
+		h.ServeHTTP(&lw, r) // обслуживание оригинального запроса
+
+		// Since возвращает разницу во времени между start
+		// и моментом вызова Since. Таким образом можно посчитать
+		// время выполнения запроса.
+		duration := time.Since(start)
+
+		// отправляем сведения о запросе в zap
+		Sugar.Infoln(
+			"uri", uri,
+			"method", method,
+			"duration", duration,
+			"status", responseData.status, // получаем перехваченный код статуса ответа
+			"size", responseData.size, // получаем перехваченный размер ответа
+			"body", string(responseData.body),
+		)
+
+	}
+	return logFn
+}
